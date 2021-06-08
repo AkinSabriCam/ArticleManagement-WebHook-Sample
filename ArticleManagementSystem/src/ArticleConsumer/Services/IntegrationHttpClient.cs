71031f55312143b4a0305baf2f278fba083d8ba5
Akın Çam <sabricam2016@gmail.com>
2021-06-08 16:34:51 +0300

    updated integration client

--- ArticleManagementSystem/src/ArticleConsumer/Services/IntegrationHttpClient.cs
+++ ArticleManagementSystem/src/ArticleConsumer/Services/IntegrationHttpClient.cs
@@ -35,16 +35,17 @@ namespace ArticleConsumer.Services
         private async Task SendArticleRequest(IntegrationArticleRequest request)
         {
             var tokenModel = await _redisManager.GetOrRunAsync(request.Code, async () => await SendTokenRequest(request));
-            var postModelContent = new StringContent(JsonSerializer.Serialize(request.ArticleModel), Encoding.UTF8,
-                  "application/json");
-            var client = _clientFactory.CreateClient();
-            client.BaseAddress = new Uri(request.Url);
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenModel.Token);
+            var postModelContent = new StringContent(JsonSerializer.Serialize(request.ArticleModel), Encoding.UTF8, "application/json");
+
+            var client = CreateClient(request.Url, tokenModel.Token);
             var response = await client.PostAsync($"/{request.EndPoint}", postModelContent);
 
             if (response.IsSuccessStatusCode)
                 _logger.LogInformation($"Article request was sent successfully - {JsonSerializer.Serialize(request.ArticleModel)}");
-            
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                await GetTokenAndSendRequestAgain(request, client, postModelContent);
+
             if (!response.IsSuccessStatusCode)
             {
                 try
@@ -71,7 +72,23 @@ namespace ArticleConsumer.Services
                 {
                     throw new Exception(ex.Message);
                 }
-            } 
+            }
+        }
+
+        private async Task GetTokenAndSendRequestAgain(IntegrationArticleRequest request, HttpClient client, StringContent postModelContent)
+        {
+            var tokenModel = await SendTokenRequest(request);
+            await _redisManager.SetAsync(request.Code, tokenModel);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenModel.Token);
+            var response = await client.PostAsync($"/{request.EndPoint}", postModelContent);
+
+            if (response.IsSuccessStatusCode)
+                _logger.LogInformation($"Article request was sent successfully - {JsonSerializer.Serialize(request.ArticleModel)}");
+            else
+                _logger.LogError($"Article could not sent successfully! Http Status Code : {response.StatusCode}");
+
+            await Task.CompletedTask;
         }
 
         private async Task<TokenResponseModel> SendTokenRequest(IntegrationArticleRequest request)
@@ -86,8 +103,7 @@ namespace ArticleConsumer.Services
 
             var postModelContent = new StringContent(JsonSerializer.Serialize(tokenModel), Encoding.UTF8,
                  "application/json");
-            var client = _clientFactory.CreateClient();
-            client.BaseAddress = new Uri(request.TokenUrl);
+            var client = CreateClient(request.TokenUrl, null);
             var response = await client.PostAsync($"/{request.TokenEndPoint}", postModelContent);
 
             if (response.IsSuccessStatusCode)
@@ -124,6 +140,18 @@ namespace ArticleConsumer.Services
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        private HttpClient CreateClient(string url, string token)
+        {
+            var client = _clientFactory.CreateClient();
+
+            client.BaseAddress = new Uri(url);
+
+            if (!string.IsNullOrEmpty(token))
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            return client;
         }
     }
 }
